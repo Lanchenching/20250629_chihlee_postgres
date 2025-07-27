16a74890ae62b4599f4a761bcfcb72131c79cc22
karenlan <karenlcj53@gmail.com>
2025-07-27 02:41:47 +0000

    modify

--- lesson8/lesson8_2.sql
+++ lesson8/lesson8_2.sql
@@ -8,3 +8,7 @@ FROM "台鐵車站資訊" LEFT JOIN "每日各站進出站人數" ON "stationCod
 
 SELECT COUNT(*) AS "筆數"
 FROM "台鐵車站資訊" LEFT JOIN "每日各站進出站人數" ON "stationCode" = "車站代碼"
+
+SELECT "stationCode","name","stationAddrTw",日期,進站人數,出站人數 
+FROM "台鐵車站資訊" LEFT JOIN "每日各站進出站人數" ON "stationCode" = "車站代碼"
+where name like '%臺北%';
