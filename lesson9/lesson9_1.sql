04bd18c933eecbfd0a669d0e08afcd8aa755a47b
karenlan <karenlcj53@gmail.com>
2025-08-03 03:06:14 +0000

    modify

--- lesson9/lesson9_1.sql
+++ lesson9/lesson9_1.sql
@@ -18,3 +18,14 @@ select "name" as 站名, count("name") as 筆數,AVG("進站人數") as "平均�
 from "每日各站進出站人數" left join "台鐵車站資訊" on "每日各站進出站人數"."車站代碼" = "台鐵車站資訊"."stationCode"
 where "日期" between '2022-01-01' and '2022-12-31'
 group by  "name" 
+
+select "name" as 站名,date_part('year',"日期") as "年份" ,count("name") as 筆數,AVG("進站人數") as "平均進站人數"
+from "每日各站進出站人數" left join "台鐵車站資訊" on "每日各站進出站人數"."車站代碼" = "台鐵車站資訊"."stationCode"
+where "日期" between '2022-01-01' and '2022-12-31'
+group by  "name","年份"
+
+select "name" as 站名,date_part('year',"日期") as "年份" ,count("name") as 筆數,AVG("進站人數") as "進站人數"
+from "每日各站進出站人數" left join "台鐵車站資訊" on "每日各站進出站人數"."車站代碼" = "台鐵車站資訊"."stationCode"
+where "name" = '基隆'
+group by  "name","年份"
+order by "進站人數" desc;
