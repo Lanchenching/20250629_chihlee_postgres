6843157270501258309142aa68088ebbf96658e8
karenlan <karenlcj53@gmail.com>
2025-08-03 02:41:47 +0000

    modify

--- lesson9/lesson9_1.sql
+++ lesson9/lesson9_1.sql
@@ -5,3 +5,16 @@ FROM "台鐵車站資訊"
 SELECT count(name) AS "台北車站數"
 FROM "台鐵車站資訊"
 WHERE "stationAddrTw" LIKE '%臺北%';
+
+select *
+from "每日各站進出站人數" left join "台鐵車站資訊" on "車站代碼" = "stationCode"
+where "stationName"= '基隆'
+
+/*
+*全省各站點2022年進站總人數
+*/
+
+select "name" as 站名, count("name") as 筆數,AVG("進站人數") as "平均進站人數"
+from "每日各站進出站人數" left join "台鐵車站資訊" on "每日各站進出站人數"."車站代碼" = "台鐵車站資訊"."stationCode"
+where "日期" between '2022-01-01' and '2022-12-31'
+group by  "name" 
