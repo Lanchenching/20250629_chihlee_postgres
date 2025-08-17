4afd1ffbeda27e39a80757bd7d3ecdef327feb58
karenlan <karenlcj53@gmail.com>
2025-08-17 02:33:30 +0000

    modify

---
+++ lesson11/lesson11_1.py
@@ -0,0 +1,17 @@
+# 當輸出df變數時,st.write()會自動執行
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+
+## streamlit為快速顯示資料給人看
